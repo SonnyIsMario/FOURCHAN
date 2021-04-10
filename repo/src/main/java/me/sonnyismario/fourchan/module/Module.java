9de9e2807ddc6028bc00b36efe42e5d87fa3a3a3
Sonny <48072766+SonnyIsMario@users.noreply.github.com>
2021-04-10 18:15:57 -0400

    boilerplate

--- repo/src/main/java/me/sonnyismario/fourchan/module/Module.java
+++ repo/src/main/java/me/sonnyismario/fourchan/module/Module.java
@@ -1,4 +1,81 @@
 package me.sonnyismario.fourchan.module;
 
+import net.minecraftforge.common.MinecraftForge;
+
 public class Module {
+
+    public String name;
+    public String desc;
+    private int key;
+    private Category category;
+    public boolean toggled;
+
+    public Module(String name, String desc, Category category) {
+        super();
+        this.name = name;
+        this.desc = desc;
+        this.key = 0;
+        this.category = category;
+        this.toggled = false;
+    }
+
+    public String getDesc()
+    {
+        return desc;
+    }
+    public void setDesc()
+    {
+        this.desc = desc;
+    }
+
+    public int getKey()
+    {
+        return key;
+    }
+
+    public void setKey(int key)
+    {
+        this.key = key;
+    }
+
+    public boolean isToggled()
+    {
+        return toggled;
+    }
+
+    public void setToggled()
+    {
+        this.toggled = toggled;
+    }
+
+    public void toggle()
+    {
+        if (this.toggled)
+        {
+            onEnable();
+        }
+        else
+        {
+            onDisable();
+        }
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public Category getCategory()
+    {
+        return this.category;
+    }
+
+    public void onEnable()
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    public void onDisable()
+    {
+        MinecraftForge.EVENT_BUS.unregister(this);
+    }
 }
